49ccd46f0ae795440762f9122e624dc11817b50b
Dainyleen Masangkay <dainyleen@gmail.com>
2021-06-23 18:31:19 +1200

    Starting a new one

--- js/main.js
+++ js/main.js
@@ -1,7 +1,1 @@
-const myHeading = document.getElementById('myHeading')
-const myButton = document.getElementById('myButton')
-const myTextInput = document.getElementById('myTextInput')
 
-myButton.addEventListener('click', () => {
-  myHeading.style.color = myTextInput.value
-})
