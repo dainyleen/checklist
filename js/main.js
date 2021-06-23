fea462fd11ae0a3c85e2f2905673e236e79ea878
Dainyleen Masangkay <dainyleen@gmail.com>
2021-06-23 18:51:31 +1200

    Practices using querySelector and querySelectorAll

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-
-const myList = document.getElementsByTagName('li')
-
-for (let i = 0; i < myList.length; i++) {
-  myList[i].style.color = '#52bab3'
-}
+const input = document.querySelector('input')
+const p = document.querySelector('p.description')
+const button = document.querySelector('button')
