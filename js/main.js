663ff88f1473a1c75460841b3fa276773c24507a
Dainyleen Masangkay <dainyleen@gmail.com>
2021-06-23 23:07:55 +1200

    Added a ADD button

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
 const toggleList = document.getElementById('toggleList')
 const listDiv = document.querySelector('.list')
-const input = document.querySelector('input.description')
-const p = document.querySelector('p.description')
-const button = document.querySelector('button.description')
+const addItemInput = document.querySelector('input.addItemInput')
+const addItemButton = document.querySelector('button.addItemButton')
 
 toggleList.addEventListener('click', () => {
   if (listDiv.style.display == 'none') {
-    toggleList.textContent = 'Hide List'
+    toggleList.textContent = 'HIDE LIST'
     listDiv.style.display = 'block'
   } else {
-    toggleList.textContent = 'Show List'
+    toggleList.textContent = 'SHOW LIST'
     listDiv.style.display = 'none'
   }
+})
+
+addItemButton.addEventListener('click', () => {
+  let li = document.createElement('li')
+  li.textContent = addItemInput.value
 })
 
