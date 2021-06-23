d068642260997e57555ab3c2d67cfffa34862a61
Dainyleen Masangkay <dainyleen@gmail.com>
2021-06-23 20:35:07 +1200

    Added a hide list button

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+const toggleList = document.getElementById('toggleList')
+const listDiv = document.querySelector('.list')
 const input = document.querySelector('input')
 const p = document.querySelector('p.description')
 const button = document.querySelector('button')
+
+toggleList.addEventListener('click', () => {
+  if (listDiv.style.display == 'none') {
+    listDiv.style.display = 'block'
+  } else {
+    listDiv.style.display = 'none'
+  }
+})
+
